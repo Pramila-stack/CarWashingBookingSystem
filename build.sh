1a5f30643617246699c39d7ce68c88ed53f15505
Pramila Tamang <pramilatmg.np@gmail.com>
2026-06-25 21:15:39 +0545

    Remove hardcoded superuser from build script

--- build.sh
+++ build.sh
@@ -7,12 +7,3 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
-
-python manage.py shell -c "
-from django.contrib.auth.models import User
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'pramilatmg.np@gmail.com', 'SparkleWash@2024')
-    print('Superuser created.')
-else:
-    print('Superuser already exists.')
-"
