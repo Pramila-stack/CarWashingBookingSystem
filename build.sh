3c182623af620c2fd458e2c1ab5f974b1f04a821
Pramila Tamang <pramilatmg.np@gmail.com>
2026-06-25 21:10:43 +0545

    Create superuser automatically on deploy

--- build.sh
+++ build.sh
@@ -7,3 +7,12 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'pramilatmg.np@gmail.com', 'SparkleWash@2024')
+    print('Superuser created.')
+else:
+    print('Superuser already exists.')
+"
